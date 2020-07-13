8b28634c3ddb6c63eac3e859394f0ab7b08dee10
altyn <altmurzilka@gmail.com>
2020-07-13 22:37:17 +0600

    comfort commit

---
+++ Neumorphism/Neumorphism/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Neumorphism
+//
+//  Created by Алтын on 7/13/20.
+//
+
+import SwiftUI
+
+extension Color {
+    static let offWhite = Color(red: 225 / 255, green: 225 / 255, blue: 235 / 255)
+}
+
+struct SimpleButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding(30)
+            .background(Circle()
+                            .fill(Color.offWhite))
+            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+            .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.offWhite
+            
+            Button(action: {
+                print("Button tapped")
+            }) {
+                Image(systemName: "heart.fill")
+                    .foregroundColor(.gray)
+            }
+            .buttonStyle(SimpleButtonStyle())
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
