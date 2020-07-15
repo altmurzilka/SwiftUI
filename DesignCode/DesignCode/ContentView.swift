f3443e3316bdfb190bc401c79fa4a67d12126ccc
altyn <altmurzilka@gmail.com>
2020-07-15 23:03:36 +0600

    commit

---
+++ DesignCode/DesignCode/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Алтын on 7/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
